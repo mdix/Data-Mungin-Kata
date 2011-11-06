712e5cecabe07fd6b232bd9d3a4a045fa35450d9
Marc Dix <dix@mediaman.de>
2011-11-06 01:11:39 +0100

    Started the kata again, made a simpler and more specialized version

--- Fileparser.php
+++ Fileparser.php
@@ -1,83 +1,58 @@
 <?php
 class Fileparser {
-    private $fileContents;
-    private $tableHeadlineLineNumber;
-    private $fromLineNumber;
-    private $untilLineNumber;
-    private $blueprint;
-    private $tableAsArray = array();
+    public $fileContents = NULL;
+    public $dayAndTemperatures = array();
     
-    public function __construct($path, $tableHeadlineLineNumber, $fromLineNumber, $untilLineNumber) {
-        if (!is_file($path)) { 
-            throw new Exception('No file at ' . $path);
-        }
-
-        $this->fileContents            = file($path);
-        $this->tableHeadlineLineNumber = --$tableHeadlineLineNumber;
-        $this->fromLineNumber          = --$fromLineNumber;
-        $this->untilLineNumber         = --$untilLineNumber;
-
-        $this->blueprint               = $this->buildBlueprint();
-        $this->mapDataFromRows();
+    public function __construct($file) {
+        $this->fileContents = $this->readDatFile($file);
+        $this->extractNeededValues();
     }
+    
+    public function readDatFile($file) {
+        return file($file);
+    }
+    
+    public function extractNeededValues() {
+        foreach ($this->fileContents as $line) {
+            preg_match('/^\s*\d+\s*\d+.\s*\d+./', $line, $elem);
+            if (!empty($elem)) {
+                $elem[0] = trim($elem[0]);
+                $unfilteredValues = explode(' ', $elem[0]);
 
-    // build blueprint array
-    private function buildBlueprint() {
-        $headlinePartsUnfiltered = explode(' ', $this->fileContents[$this->tableHeadlineLineNumber]);
-        $headlinePartsUnordered  = array_filter($headlinePartsUnfiltered);
+                // filter empty values and remove non digits (like asteriks)
+                $values = array();
+                foreach ($unfilteredValues as $value) {
+                    if (!empty($value)) {
+                        array_push($values, intval($value));
+                    }
+                }
+                
+                array_push($this->dayAndTemperatures, $values);
+                unset($unfilteredValues, $value, $values, $elem);
+            }
+        }
+        unset($line);
+    }
+    
+    public function getDayWithSmallestTemperatureSpread() {
+        $smallestSpread = 9999;
+        $day = false;
         
-        // need numeric values
-        foreach ($headlinePartsUnordered as $key => $value) {
-            $headlineParts[] = $value;
-        }
-
-        // values to keys
-        $headlineParts = array_flip($headlineParts);
-        return $headlineParts;
-    }
-
-    private function mapDataFromRows() {
-        $start = $this->fromLineNumber;
-        $line  = $this->fromLineNumber;
-        $end   = $this->untilLineNumber;
-
-        for ($start; $line <= $end; $line++) {
-            $rowPartsUnfiltered = explode(' ', $this->fileContents[$line]);
-            $rowPartsUnordered  = array_filter($rowPartsUnfiltered);
-            $rowParts           = array();
-            // need numeric values
-            foreach ($rowPartsUnordered as $key => $value) {
-                array_push($rowParts, $value);
+        foreach ($this->dayAndTemperatures as $key => $values) {
+            $spread = $this->calculateTemperatureSpread($key);
+            if ($spread < $smallestSpread) {
+                $smallestSpread = $spread;
+                $day = $this->dayAndTemperatures[$key][0];
             }
-
-            $filledBlueprint = $this->blueprint;
-
-            $i = 0;
-            foreach ($filledBlueprint as $key => $value) {
-                if (!empty($rowParts[$i])) {
-                    $filledBlueprint[$key] = $rowParts[$i];
-                    $i++;
-                }
-            }
-
-            array_push($this->tableAsArray, $filledBlueprint);
-        }
-    }
-
-    public function getDayNumberWithSmallestTemperatureSpread() {
-        $day    = NULL;
-        $spread = NULL;
-
-        foreach ($this->tableAsArray as $key => $cellname) {
-                $newSpread = $cellname['MxT'] - $cellname['MnT'];
-
-                if ($spread === NULL || $spread > $newSpread) {
-                    $spread = $newSpread;
-                    $day    = $cellname['Dy'];
-                }
         }
         
         return $day;
+    }
+    
+    public function calculateTemperatureSpread($key) {
+        $maxTemperature = $this->dayAndTemperatures[$key][1];
+        $minTemperature = $this->dayAndTemperatures[$key][2];
+        return $maxTemperature - $minTemperature;
     }
 
 }
--- tests/FileparserTest.php
+++ tests/FileparserTest.php
@@ -5,15 +5,51 @@ class FileparserTest extends PHPUnit_Framework_TestCase {
     private $fileparserObj;
 
     protected function setUp() {
-        $this->fileparserObj = new Fileparser('../weather.dat', 7, 9, 38);
+        $this->fileparserObj = new Fileparser('../weather.dat');
     }
 
-    public function test__construct() {
-        $this->assertInstanceof('Fileparser', $this->fileparserObj);
+    public function testReadDatFile() {
+        $this->assertNotNull($this->fileparserObj->fileContents);
+        $this->assertInternalType('array', $this->fileparserObj->fileContents);
+    }
+    
+    public function testExtractNeededValues() {
+        // wanna have a two dimensional array
+        $this->assertInternalType('array', $this->fileparserObj->dayAndTemperatures);
+        $this->assertInternalType('array', $this->fileparserObj->dayAndTemperatures[0]);
+        
+        $this->assertEquals(1, $this->fileparserObj->dayAndTemperatures[0][0]);   // day 1
+        $this->assertEquals(88, $this->fileparserObj->dayAndTemperatures[0][1]);  // max
+        $this->assertEquals(59, $this->fileparserObj->dayAndTemperatures[0][2]);  // min
+        
+        $this->assertEquals(13, $this->fileparserObj->dayAndTemperatures[12][0]); // day 13
+        $this->assertEquals(70, $this->fileparserObj->dayAndTemperatures[12][1]); // max
+        $this->assertEquals(59, $this->fileparserObj->dayAndTemperatures[12][2]); // min
+
+        $this->assertEquals(9, $this->fileparserObj->dayAndTemperatures[8][0]); // day 9
+        $this->assertEquals(86, $this->fileparserObj->dayAndTemperatures[8][1]); // max
+        $this->assertEquals(32, $this->fileparserObj->dayAndTemperatures[8][2]); // min (has a asteriks)
+
+        $this->assertEquals(26, $this->fileparserObj->dayAndTemperatures[25][0]); // day 26
+        $this->assertEquals(97, $this->fileparserObj->dayAndTemperatures[25][1]); // max (has a asteriks)
+        $this->assertEquals(64, $this->fileparserObj->dayAndTemperatures[25][2]); // min
+        
+        $this->assertEquals(30, $this->fileparserObj->dayAndTemperatures[29][0]); // day 30 (last day)
+        $this->assertEquals(90, $this->fileparserObj->dayAndTemperatures[29][1]); // max
+        $this->assertEquals(45, $this->fileparserObj->dayAndTemperatures[29][2]); // min
+        
+        $this->assertArrayNotHasKey(30, $this->fileparserObj->dayAndTemperatures); // proper end
     }
 
-    public function testGetDayNumberWithSmallestTemperatureSpread() {
-        $this->assertEquals(14, $this->fileparserObj->getDayNumberWithSmallestTemperatureSpread());
+    public function testGetDayWithSmallestTemperatureSpread() {
+        $this->assertEquals(14, $this->fileparserObj->getDayWithSmallestTemperatureSpread()); // should return day 14
+    }
+    
+    public function testCalculateTemperatureSpread() {
+        $this->assertEquals(16, $this->fileparserObj->calculateTemperatureSpread(1)); // day 2
+        $this->assertEquals(20, $this->fileparserObj->calculateTemperatureSpread(5)); // day 6
+        $this->assertEquals(33, $this->fileparserObj->calculateTemperatureSpread(25)); // day 26
+        $this->assertEquals(45, $this->fileparserObj->calculateTemperatureSpread(29)); // day 30
     }
 
 }
